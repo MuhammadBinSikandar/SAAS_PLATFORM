dc2a6ca0e5631a2dcab8e6f6dd51a2a0c87174f6
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-09-04 06:55:06 +0500

    Setup the ground for the subscription model

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,15 +1,55 @@
 from django.db import models
 from django.conf import settings
+import helpers.billing as billing
+from allauth.account.signals import (
+    user_signed_up as allauth_user_signed_up,
+    email_confirmed as allauth_email_confirmed,
+)
 
-User = settings.AUTH_USER_MODEL
+User = settings.AUTH_USER_MODEL # get the user model from settings file
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    strip_id = models.CharField(max_length=100, blank=True, null=True)
+    stripe_id = models.CharField(max_length=100, blank=True, null=True)
+    init_email = models.EmailField(blank=True, null=True)
+    init_email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username}"
     
     def save(self, *args, **kwargs):
-        self.strip_id = "some-random-id"
+        if not self.stripe_id:
+            if self.init_email_confirmed and self.init_email:
+                email = self.init_email
+                if email != "" or email is not None:
+                    stripe_id = billing.create_customer(
+                        raw=False, 
+                        email=email, 
+                        metadata={
+                            "user_id": self.user.id, 
+                            "username": self.user.username
+                        }
+                    )
+                    self.stripe_id = stripe_id
         super().save(*args, **kwargs)
+
+def allauth_user_signed_up_handler(request, user, *args , **kwargs):
+    email = user.email
+    Customer.objects.create(
+        user=user, 
+        init_email=email, 
+        init_email_confirmed=False
+    )
+
+allauth_user_signed_up.connect(allauth_user_signed_up_handler)
+
+def allauth_email_confirmed_handler(request, email_address, *args, **kwargs):
+    qs = Customer.objects.filter(
+        init_email=email_address, 
+        init_email_confirmed=False
+    )
+    for obj in qs:
+        obj.init_email_confirmed = True
+        obj.save() 
+
+allauth_email_confirmed.connect(allauth_email_confirmed_handler)
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_save
 from django.conf import settings
+import helpers.billing as billing
+import stripe
+from decouple import config
 
 User = settings.AUTH_USER_MODEL
 ALLOW_CUSTOM_GROUPS = True
+
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="", cast=str)
+
 
 SUBSCRIPTION_PERMISSIONS = [
             ("pro", "Pro Perm"),
@@ -15,6 +21,9 @@ SUBSCRIPTION_PERMISSIONS = [
 
 # Create your models here.
 class Subscription(models.Model):
+    """
+    Subscription Plan = Stripe Product
+    """
     name = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
@@ -23,11 +32,69 @@ class Subscription(models.Model):
         "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
         }
     )
+    stripe_id = models.CharField(max_length=100, blank=True, null=True)
     def __str__(self):
         return f"{self.name}"
     
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+    def save(self, *args, **kwargs):
+        if not self.stripe_id:
+            stripe_id = billing.create_product(
+                        name=self.name, 
+                        metadata={
+                            "subscription_plan_id": self.id, 
+                        },
+                        raw=False
+                    )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+                    
+
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe Price
+    """
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly"
+        YEARLY = "year", "Yearly" 
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)
+    stripe_id = models.CharField(max_length=100, blank=True, null=True)
+    interval = models.CharField(max_length=100, choices=IntervalChoices.choices, default=IntervalChoices.MONTHLY)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+    
+    @property
+    def stripe_price(self):
+        """ remove the decimal point and return the price in cents """
+        return int(self.price * 100)
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+    
+    def save(self, *args, **kwargs):
+        if (not self.stripe_id and self.product_stripe_id):
+            stripe.api_key = STRIPE_SECRET_KEY
+            stripe_id = billing.create_price(
+                product=self.product_stripe_id,
+                unit_amount=self.stripe_price,
+                currency=self.stripe_currency,
+                interval=self.interval,
+                metadata={"subscription_plan_price_id": self.id},
+                raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args,**kwargs)
+            
+
+    
 
 class UserSubscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # one to one relationship with the user
