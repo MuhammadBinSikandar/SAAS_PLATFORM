4c0229727cee6d343649896b8361a657c4d92857
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-09-17 15:35:16 +0500

    sync of user subs

--- src/subscriptions/management/commands/sync_user_subs.py
+++ src/subscriptions/management/commands/sync_user_subs.py
@@ -8,14 +8,25 @@ import helpers.billing as billing
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
+        parser.add_argument("--day-start", default=0, type=int)
+        parser.add_argument("--day-end", default=0, type=int)
+        parser.add_argument("--days-left", default=0, type=int)
+        parser.add_argument("--days-ago", default=0, type=int)
         parser.add_argument("--clear-dangling", action="store_true", default=False)
 
     def handle(self, *args:Any, **options:Any):
-        print(options)
+        # print(options)
+        days_left = options.get("days_left")
+        days_ago = options.get("days_ago")
+        day_start = options.get("day_start")
+        day_end = options.get("day_end")
         clear_dangling = options.get("clear_dangling")
         if clear_dangling:
             print("Clearing Dangling Subscriptions not in use in stripe account")
             sub_utils.clear_dangling_subs()
         else:
             print("Syncing active Subscriptions")
-            print("Done")
+            done = sub_utils.refresh_active_users_subscription(
+                active_only=True,days_left=days_left, days_ago=days_ago, day_start=day_start, day_end=day_end, verbose=True)
+            if done:
+                print("Done")
