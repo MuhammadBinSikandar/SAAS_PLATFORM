e51645afe6999e16cf54733b3d7ebac982b24f43
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-09-17 09:58:02 +0500

    Billing and pricing scheme adjusted

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -87,28 +87,35 @@ def get_subscription(stripe_id, raw = True):
     response = stripe.Subscription.retrieve(stripe_id)
     if raw:
         return response
-    return response.url
+    return serialize_subscription_data(response)
 
-def cancel_subscription(stripe_id, reason,feedback ,raw = True):
-    response = stripe.Subscription.cancel(
-                            stripe_id,
-                            cancellation_details={
-                                "comment": reason,
-                                "feedback": feedback
-                            }
-                        )
+def get_customer_active_subscriptions(customer_stripe_id):
+    response = stripe.Subscription.retrieve(
+            customer = customer_stripe_id,
+            status = "active",
+            )
+    return response
+
+def cancel_subscription(stripe_id, reason="", feedback="", cancel_at_period_end=False, raw = True):
+    if cancel_at_period_end:
+        response = stripe.Subscription.modify(stripe_id, cancel_at_period_end=cancel_at_period_end,cancellation_details={ "comment": reason, "feedback": feedback })
+    else:
+        response = stripe.Subscription.cancel(stripe_id, cancellation_details={ "comment": reason, "feedback": feedback })
+    
     if raw:
         return response
-    return response.url
+    return serialize_subscription_data(response)
 
 def serialize_subscription_data(subscription_response):
     status = subscription_response.status
     current_period_start = date_utils.timestamp_as_datetime(subscription_response.current_period_start)
     current_period_end = date_utils.timestamp_as_datetime(subscription_response.current_period_end)
+    cancel_at_period_end = subscription_response.cancel_at_period_end
     return {
         "status": status,
         "current_period_start": current_period_start,
-        "current_period_end": current_period_end
+        "current_period_end": current_period_end,
+        "cancel_at_period_end": cancel_at_period_end,
     }
 
 def get_checkout_customer_plan(session_id):
