3c7b6dc6d6e411e1b88665b50f62904c2a4c0e79
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-09-17 13:32:44 +0500

    permissions sync using utility functions

--- src/subscriptions/management/commands/sync_permissions.py
+++ src/subscriptions/management/commands/sync_permissions.py
@@ -1,18 +1,10 @@
 from django.core.management.base import BaseCommand
 from typing import Any
 from subscriptions.models import Subscription
+from subscriptions import utils as sub_utils
 
 
 class Command(BaseCommand):
 
     def handle(self, *args:Any, **kwargs:Any):
-        # print('Hello World')
-        qs = Subscription.objects.filter(active=True)
-        for obj in qs:
-            # print(obj.groups.all())
-            sub_perms = obj.permissions.all()
-            for group in obj.groups.all():
-                group.permissions.set(sub_perms)
-                # for per in group.permissions.all():
-                #     group.permissions.add(per)
-            # print(obj.permissions.all())
+        sub_utils.sync_subs_groups_permissions()
