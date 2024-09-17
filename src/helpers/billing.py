bb7df28674bbf8c2e36f6f9d6af21c60db818584
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-09-17 13:23:28 +0500

    Billing adjustments

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -90,10 +90,10 @@ def get_subscription(stripe_id, raw = True):
     return serialize_subscription_data(response)
 
 def get_customer_active_subscriptions(customer_stripe_id):
-    response = stripe.Subscription.retrieve(
-            customer = customer_stripe_id,
-            status = "active",
-            )
+    response =  stripe.Subscription.list(
+            customer=customer_stripe_id,
+            status="active"
+        )
     return response
 
 def cancel_subscription(stripe_id, reason="", feedback="", cancel_at_period_end=False, raw = True):
